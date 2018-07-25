8730018dfde7be843fe6158debc45cb79597ea40
James Sandoval <gunnisurf@gmail.com>
2018-07-25 06:43:16 -0700

    Initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+  	new Recipe('Test Recipe', 'Test Desciption Lorem Ipsum Dolor Sit amet paracatem biglor. El Vacor tas liberatum conspicatoru delicouso.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70'),
+    new Recipe('Test Recipe 2', 'Test Desciption #2, This should be something else entirely. Very Cool.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F06%2Fmain%2Frigatoni-kale-pesto-1708p12.jpg%3Fitok%3DZRmUV5oK&w=700&q=85')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+}
